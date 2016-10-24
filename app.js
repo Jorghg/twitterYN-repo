e5166f27079eed121bfb46034af4beabf0c85f2d
ingrskar <ingrskar@stud.ntnu.no>
2016-10-24 12:09:39 +1000

    live tweets sent to client

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.io = require('socket.io')();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', routes(app.io));
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,85 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function (io) {
 
-var sentiment = require('sentiment');
-var Twitter = require('twitter');
+    var express = require('express');
+    var router = express.Router();
 
-var client = new Twitter({
-    consumer_key: 'XoP0cUhbC3KIzM5lqkkQpn06N',
-    consumer_secret: 'k0ImtPjZ6iN1DwIZaBZrHuLIxxhtnRMaeycIreKJ9AN2JFu7sP',
-    access_token_key: '1917108290-38rqsxvEuJmXKOmBrsqHuygsO09MBTTJrFr5l3Z',
-    access_token_secret: 'bY1tBzx9BgF5PYlsm8DQmCEGRnkj1SMO6w4lJsbcjePun'
-});
+    var sentiment = require('sentiment');
+    var Twitter = require('twitter');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Trump, Yey or Ney?' });
-});
-
-
-var resultTweets = []; // shouln't be global, change later, note to niggrid
-var positive = 0;
-var negative = 0;
-var neutral= 0;
-var posProsent = 0;
-var negProsent = 0;
-var neutralProsent = 0;
-var resultAnalysis = [];
-
-var twitterSearch = function (inputTweet) {
-    client.stream('statuses/filter', {track: inputTweet},  function(stream) {
-        stream.on('data', function(tweet) {
-            var sentimentTweet = sentiment(tweet.text);
-            var sentimentScore = sentimentTweet.score;
-            var resultTweet = [{tweet: tweet.text, score: sentimentScore}]
-
-            resultTweets.push({tweet: tweet.text, score: sentimentScore}); /// add result to list
-
-            sentimentAnalysis(sentimentScore)
-
-        });
-        stream.on('error', function(error) {
-            console.log(error);
-        });
+    var client = new Twitter({
+        consumer_key: 'XoP0cUhbC3KIzM5lqkkQpn06N',
+        consumer_secret: 'k0ImtPjZ6iN1DwIZaBZrHuLIxxhtnRMaeycIreKJ9AN2JFu7sP',
+        access_token_key: '1917108290-38rqsxvEuJmXKOmBrsqHuygsO09MBTTJrFr5l3Z',
+        access_token_secret: 'bY1tBzx9BgF5PYlsm8DQmCEGRnkj1SMO6w4lJsbcjePun'
     });
+
+    /* GET home page. */
+    router.get('/', function(req, res, next) {
+        res.render('index', { title: 'Trump, Yey or Ney?' });
+    });
+
+
+    //note to self, disse burde endres på
+    var resultTweets = [];
+    var positive = 0;
+    var negative = 0;
+    var neutral= 0;
+    var posProsent = 0;
+    var negProsent = 0;
+    var neutralProsent = 0;
+    var resultAnalysis = [];
+
+    var twee = io.of('liveTweet');
+
+    var twitterSearch = function (inputTweet) {
+        client.stream('statuses/filter', {track: inputTweet},  function(stream) {
+            stream.on('data', function(tweet) {
+                var sentimentTweet = sentiment(tweet.text);
+                var sentimentScore = sentimentTweet.score;
+                var resultTweet = [{tweet: tweet.text, score: sentimentScore}]
+
+                resultTweets.push({tweet: tweet.text, score: sentimentScore}); /// add result to list
+
+                sentimentAnalysis(sentimentScore)
+
+                io.emit('liveTweet',tweet.text);
+
+            });
+            stream.on('error', function(error) {
+                console.log(error);
+            });
+        });
+    };
+
+    var sentimentAnalysis = function (score) {
+
+        if(score>0) {
+            positive +=1;
+        }
+        if(score<0) {
+            negative +=1;
+        }
+        if(score==0){
+            neutral +=1;
+        }
+
+        posProsent = (positive/resultTweets.length)*100;
+        negProsent = (negative/resultTweets.length)*100;
+        neutralProsent = (neutral/resultTweets.length)*100;
+
+        resultAnalysis.push({positive:posProsent, negative: negProsent, neutral: neutralProsent});
+        console.log(resultAnalysis)
+    };
+
+    var main = function() {
+        twitterSearch("trump");
+    };
+
+
+    main();
+
+    return router;
 };
 
-var sentimentAnalysis = function (score) {
-
-    if(score>0) {
-        positive +=1;
-    }
-    if(score<0) {
-        negative +=1;
-    }
-    if(score==0){
-        neutral +=1;
-    }
-
-    posProsent = (positive/resultTweets.length)*100;
-    negProsent = (negative/resultTweets.length)*100;
-    neutralProsent = (neutral/resultTweets.length)*100;
-
-    resultAnalysis.push({positive:posProsent, negative: negProsent, neutral: neutralProsent});
-    console.log(resultAnalysis)
-};
-
-var main = function() {
-    twitterSearch("trump");
-};
 
 
-main();
-
-
-
-module.exports = router;
