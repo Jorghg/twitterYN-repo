35b000ce75426b37d57d456edcab45ee3a1357dc
ingrskar <ingrskar@stud.ntnu.no>
2016-11-02 16:55:54 +1000

    funk da

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ module.exports = function (io) {
         });
 
     var docClient = new AWS.DynamoDB.DocumentClient();
+
     var table = "TrumpStats";
 
     //twitter cresidentals
@@ -29,7 +30,7 @@ module.exports = function (io) {
     });
 
 
-    // classifier
+    // classifier for sentiment
     var Classifier = '';
 
     //global variables...
@@ -56,36 +57,14 @@ module.exports = function (io) {
     var clintonPosPer = '';
     var clintonNegPer = '';
 
-    var timestamp = '';
-
-    //Parameters for put and query Can be made into two different if we need to.
-    var paramsPut = {
-        TableName:table,
-        Item: {
-            "timestamp": Date.now(),
-            "stats": {
-                "resultTrump": resultTrump,
-                "resultClinton": resultClinton,
-                "resultObama": resultObama
-            }
-        }
-    };
-
-    var paramsQuery = {
-        TableName: table,
-        KeyConditionExpression: "#searchNumber = :number",
-        ExpressionAttributeNames: {
-            "#searchNumber": "timestamp"
-        },
-        ExpressionAttributeValues: {
-            ":number": 1477990335014
-        }
-
-    };
+    // HUSK!!!!
+    var dbID = 9;
+    var ready = false;
 
     //define stream
     var stream = twit.stream('statuses/filter',{language:'en',track: 'trump,clinton,obama'});
 
+    // load classifier
     natural.BayesClassifier.load('classifier.json', null, function(err, classifier) {
         Classifier = classifier;
     });
@@ -95,19 +74,48 @@ module.exports = function (io) {
 
         socketId = socket.id;
 
+        // if no sockets is connected, load from db and start stream
         if(socketConnected == 0){
+            var paramsQuery = {
+                TableName: table,
+                KeyConditionExpression: "#searchNumber = :number",
+                ExpressionAttributeNames: {
+                    "#searchNumber": "ID"
+                },
+                ExpressionAttributeValues: {
+                    ":number": dbID
+                }
+
+            };
             docClient.query(paramsQuery, function(err, data) {
                 if (err) {
                     console.error("Unable to query. Error: ", JSON.stringify(err, null, 2));
                 }  else  {
                     console.log("Query succeeded. ");
+
                     data.Items.forEach(function(item) {
-                        console.log("Here we print what ever we put it"," + item.WHATEVER WE WHANT TO PRINT");
+                        posTrump = item.stats.posTrump;
+                        resultTrump = item.stats.countTrump;
+                        negTrump = resultTrump - posTrump;
+
+                        posClinton = item.stats.posClinton;
+                        resultClinton = item.stats.countClinton;
+                        negClinton = resultClinton - posClinton;
+
+                        posObama = item.stats.posObama;
+                        resultObama = item.stats.countObama;
+                        negObama = resultObama - posObama;
+                        ready = true;
+                        console.log('goodshit');
+
                     });
                 }
             });
-            stream.start();
-            console.log('started');
+            if (ready == true) {
+                stream.start();
+                console.log('started');
+            }
+
         }
 
         socketConnected += 1;
@@ -115,42 +123,35 @@ module.exports = function (io) {
         console.log('Sockets connected: ' + socketConnected);
 
 
-        //catch tweet
-        stream.on('tweet',function(tweet) {
-            var inTweet = tweet.text.toLowerCase();
-            if (inTweet.includes('trump')){
-                trump(tweet);
-                io.emit('trumpTweet',resultTweetTrump);
-            } else if (inTweet.includes('clinton')) {
-                clinton(tweet);
-                io.emit('clintonTweet',resultTweetClinton);
-            } else if (inTweet.includes('obama')){
-                obama(tweet);
-                io.emit('obamaTweet',resultTweetObama);
-            }
-            statusTweet();
-            io.emit('status',yey);
-
-        });
-
-        // catch error
-        stream.on('error', function(error) {
-            console.log('Stream error: ' + error.message);
-        });
-
-
         socket.on('disconnect', function () {
             socketConnected -= 1;
             console.log('Disconnect');
             console.log('Sockets connected:' + socketConnected);
+            dbID += 1;
+            // if no sockets are connected, put into db
             if (socketConnected == 0){
+                ready = false;
                 stream.stop();
                 console.log('stopped');
+                var paramsPut = {
+                    TableName:table,
+                    Item: {
+                        "ID": dbID,
+                        "stats": {
+                            "posTrump": posTrump,
+                            "posClinton": posClinton,
+                            "posObama": posObama,
+                            "countTrump": resultTrump,
+                            "countClinton": resultClinton,
+                            "countObama": resultObama
+                        }
+                    }
+                };
                 docClient.put(paramsPut, function(err, data) {
                     if (err) {
                         console.log("Unable to add to DB: Error JSON", JSON.stringify(err, null, 2));
                     }  else  {
-                        console.log("Items added to DB: ", JSON.stringify(data, null, 2));
+                        console.log("Items added to DB: ", data);
                     }
                 });
             }
@@ -159,6 +160,32 @@ module.exports = function (io) {
     });
 
 
+    //catch tweet
+    stream.on('tweet',function(tweet) {
+        var inTweet = tweet.text.toLowerCase();
+        if (inTweet.includes('trump')){
+            trump(tweet);
+            io.emit('trumpTweet',resultTweetTrump);
+        } else if (inTweet.includes('clinton')) {
+            clinton(tweet);
+            io.emit('clintonTweet',resultTweetClinton);
+        } else if (inTweet.includes('obama')){
+            obama(tweet);
+            io.emit('obamaTweet',resultTweetObama);
+        }
+        statusTweet();
+        io.emit('status',yey);
+
+    });
+
+    // catch error
+    stream.on('error', function(error) {
+        console.log('Stream error: ' + error.message);
+    });
+
+
+
+    // Analyse Obama tweets
     var obama = function(tweet) {
         var sentence = '';
         var tweetText = tweet.text;
@@ -172,13 +199,14 @@ module.exports = function (io) {
 
         resultObama += 1;
 
-        calcObama(sentValue);
+        calcObama(sentValue); // call calculate function
 
+        // set result
         resultTweetObama = {tweetID:tweet.id_str, positive:obamaPosPer, negative: obamaNegPer, count:resultObama};
 
     };
 
-    // analyse trump tweets
+    // analyse Trump tweets
     var trump = function(tweet) {
         var sentence = '';
         var tweetText = tweet.text;
@@ -192,12 +220,13 @@ module.exports = function (io) {
 
         resultTrump += 1;
 
-        calcTrump(sentValue);
+        calcTrump(sentValue); // calculate function on sentiment value
 
+        // set result
         resultTweetTrump = {tweetID:tweet.id_str, positive:trumpPosPer, negative: trumpNegPer, count: resultTrump};
     };
 
-    // analyse searched tweets
+    // analyse clinton tweets
     var clinton = function(tweet) {
         var sentence = '';
         var tweetText = tweet.text;
@@ -217,7 +246,7 @@ module.exports = function (io) {
 
     };
 
-    // percent total
+    // calculate Obama percentage
     var calcObama = function (score) {
 
         if(score==4) {
@@ -233,7 +262,7 @@ module.exports = function (io) {
 
     };
 
-    // percent trump tweets
+    // calculate Trump percentage
     var calcTrump = function (score) {
 
         if(score==4) {
@@ -248,7 +277,7 @@ module.exports = function (io) {
         trumpNegPer = Math.round((negTrump/resultTrump)*100);
     };
 
-    // percent seached tweets
+    // calculte clinton percentage
     var calcClinton = function (score) {
 
         if(score==4) {
@@ -264,14 +293,19 @@ module.exports = function (io) {
 
     };
 
+
     var statusTweet = function () {
         if(trumpPosPer > clintonPosPer && trumpPosPer > obamaPosPer) {
-            yey = {trump: true, clinton:false, obama:false};
+            yey = {trump: true, clinton:false, obama:false, tc: false, to: false};
+        } else if (trumpPosPer > clintonPosPer) {
+            yey = {trump: false, clinton:false, obama:false, tc: true, to: false}
+        } else if (trumpPosPer > obamaPosPer) {
+            yey = {trump: false, clinton:false, obama:false, tc: false, to: true}
         } else {
            if (clintonPosPer > obamaPosPer) {
-               yey = {trump:false, clinton:true, obama:false};
+               yey = {trump:false, clinton:true, obama:false, tc: false, to: false};
            } else {
-               yey = {trump:false, clinton:false, obama:true};
+               yey = {trump:false, clinton:false, obama:true, tc: false, to: false};
            }
         }
     };
